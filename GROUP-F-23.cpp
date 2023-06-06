f668fe8b9ed460d550547f00334fd452d8db1321
Sayali Rahane <113023490+RahaneSayali@users.noreply.github.com>
2023-06-06 11:23:51 +0530

    Added sequential file inserted

--- GROUP-F-23.cpp
+++ GROUP-F-23.cpp
@@ -7,111 +7,133 @@
  If record of student does not exist an appropriate message is displayed. If it is, then the system displays 
  the student details. Use sequential file to main the data.
  */
-//Header files 
+
 #include <iostream>
+#include<fstream>
+#include<iomanip>
+
 using namespace std;
 
-struct Node  //NOde structure For BST
+void addStudent () 
 {
-    int marks;
-    struct Node *l;
-    struct Node *r;
-} *r;
+ofstream f("db.txt", ios::app);
+ string rn,name, div, add;
 
-struct Node *createNode()   //create node function
-{
-    struct Node *tmp;
-    tmp = new struct Node;
-    cout << "\n\t Enter Marks : ";
-    cin >> tmp->marks;
-    tmp->l = NULL;
-    tmp->r = NULL;
-    return tmp;
+cout << "Enter student Roll No: ";
+cin>>rn;
+cout<< "Enter Student Name:" ;
+cin>> name;
+cout << "Enter student division:";
+cin>>div;
+cout <<"Enter Student Address:";
+cin >> add;
+f<<setw(20)<<rn<<setw (20)<< setw(20)<<name<< setw (20)<<div<<setw(20)<<add;
+
+f.close();
+
+cout<<"Student added successfully";
 }
 
-void createBST()    create root functions
+void printStudent()
 {
-    if (r == NULL)
-    {
-        r = createNode();
-        cout << "\n\t ## Root of BST Tree is created ##";
-    }
-}
-//insert nodes in BST function
-void insert(struct Node *Root, struct Node *newnode)
+
+ifstream f ("db.txt");
+
+string line ;
+cout<< " \n printing file Data..."<<endl;
+
+while (getline (f, line)) 
 {
-    if (newnode->marks < Root->marks)
-    {
-        if(Root->l == NULL)
-        {
-            Root->l = newnode;
-        }
-        else
-        {
-            insert(Root->l,newnode);
-        }
-    }
-    else
-    {
-        if(Root->r == NULL)
-        {
-            Root->r = newnode;
-        }
-        else
-        {
-            insert(Root->r,newnode);
-        }
-    }
-}
-// void preorder traversal function
-void preorder(struct Node *Root)
-{
-    if(Root != NULL)
-    {
-        cout<<" "<<Root->marks;
-        preorder(Root->l);
-        preorder(Root->r);
-    }
-}
-//function to convert BST into MAX Heap Tree
-void maxHeapify(struct Node *Parent)
-{
-    int val;
-    val = Parent->r->marks;
-    Parent->r->marks = Parent->marks;
-    Parent->marks = val;
+cout << line <<endl;
 }
 
-int main()   //main function
+cout << " printing completed!";
+ f. close();
+}
+
+void SearchStudent ()
 {
-    int cnt,i,j;
-    struct Node *newnode;
-    cout<<"\n\n ## Create and Display MAX Heap Tree";
-    r = NULL;
-    i = j = 0;
-    createBST();   //step1 create BST
-    //step 2 insert nodes in BST
-    cout<<"\n\n How many nodes to insert ? : ";
-    cin>>cnt;
-    for(i=0;i<cnt;i++)
-    {
-        newnode = createNode();
-        insert(r,newnode);
-    }
-    cout<<"\n\n Preorder Traversal : ";  //step3 display BST
-    preorder(r);
+ ifstream f("db.txt") ;
 
-//step 4 convert BST into MAX Heap Tree
-    maxHeapify(r);
-    maxHeapify(r->l);
-    maxHeapify(r->r);
-    maxHeapify(r);
-//step 5 display MAX Heap Tree
-    cout<<"\n\n After Heapifying BST ";
-    cout<<"\n\n Max Heap Tree Preorder Traversal : ";
-    preorder(r);
+string line,rn;
 
-    cout<<"\n\n Maximum Marks : "<<r->marks;
-    cout<<"\n\n";
-    return 0;
+cout<< "Enter Shident Roll No. To be search:";
+cin>>rn;
+
+bool found = false;
+ while (getline (f,line)) {
+if(line.find(rn)!=string::npos)
+{
+	cout<<"student details:"<<endl;
+	cout<<line<<endl;
+	found=true;
+	break;
+}
+}
+f.close();
+if(!found)
+{cout << " Student Doesn't Exist "<< endl;
+}
+
+}
+
+void deletestudent () 
+{ ifstream f("db.txt");
+
+string line,rn;
+
+cout << " Enter Student Roll no. To deleted";
+cin>>rn;
+
+string fileData;
+
+while(getline (f, line))
+ { if (line.find (rn) == string::npos) 
+ { fileData += line;
+fileData += " \n";
+}
+}
+}
+
+int main()
+{
+  ofstream f("db.txt", ios:: out); 
+ f<< left << setw(20) <<" Roll No" <<setw(20)<<" NAME " << setw(20)<<"Division"<<setw(20)<<"Address"<<endl;
+ f.close();
+ int ip;
+ while(ip!=-1)
+{ cout << "\n Enter your choice \n. 1.Add student\n 2. Delete Student \n 3.Search Student\n4.printdata\n5"<<endl;
+
+cin>> ip;
+
+switch (ip)
+ { case 1:
+ 	addStudent();
+	break; 
+
+case 2:
+
+	deletestudent();
+	break; 
+
+case 3:
+ 	SearchStudent ();
+ 	break; 
+
+case 4:
+
+	printStudent();
+	break; 
+case 5:
+	return 0;
+	break;
+
+default:
+
+cout<< "please Refnter your choice"<<endl;
+break;
+}
+
+}
+return 0;
 }
