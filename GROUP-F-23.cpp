7e50dba88989f2b80f3af6652e98a126f4710428
Sayali Rahane <113023490+RahaneSayali@users.noreply.github.com>
2023-06-06 21:15:51 +0530

    some changes

--- GROUP-F-23.cpp
+++ GROUP-F-23.cpp
@@ -8,130 +8,118 @@
  the student details. Use sequential file to main the data.
  */
 
-#include <iostream>
+#include<iostream>
 #include<fstream>
 #include<iomanip>
-
 using namespace std;
-void addStudent () 
+void addstudent()
 {
-ofstream f("db.txt", ios::app);
- string rn,name, div, add;
+    ofstream f("db.txt",ios::app);
+    string rn,div,name,add;
+    cout<<"\n enter the roll no of student:";
+    cin>>rn;
+    cout<<"\n enter the division of student:";
+    cin>>div;
+    cout<<"\n enter the name of student:";
+    cin>>name;
+    cout<<"\n enter the address of student:";
+    cin>>add;
+    f<<setw(20)<<rn<<setw(20)<<setw(20)<<div<<setw(20)<<name<<setw(20)<<add<<endl;
+    f.close();
+    cout<<"\n student added successfully!!!";
 
-cout << "Enter student Roll No: ";
-cin>>rn;
-cout<< "Enter Student Name:" ;
-cin>> name;
-cout << "Enter Student Division:";
-cin>>div;
-cout <<"Enter Student Address:";
-cin >> add;
-f<<setw(20)<<rn<<setw (20)<< setw(20)<<name<< setw (20)<<div<<setw(20)<<add;
 
-f.close();
-
-cout<<"Student added successfully";
 }
-
-void printStudent()
+void printstudent()
 {
-
-ifstream f ("db.txt");
-string line ;
-cout<< " \n printing file Data..."<<endl;
-while (getline (f, line)) 
+    ifstream f("db.txt");
+    string line;
+    cout<<"\n printing data of student:";
+    while(getline(f,line))
+    {
+        cout<<line<<endl;
+    }
+    cout<<"printing is completed!!";
+    f.close();
+}
+void searchstudent()
 {
-cout << line <<endl;
+    ifstream f("db.txt");
+    string line,rn;
+    cout<<"\n enter the roll to be search:";
+    cin>>rn;
+    bool found=false;
+    while(getline(f,line))
+    {
+        if(line.find (rn)!=string::npos)
+        {
+            cout<<"\n student found "<<endl;
+            cout<<"student details!!!"<<endl;
+            cout<<line<<endl;
+            found=true;
+            break;
+        }
+    }
+    f.close();
+    if(!found)
+    {
+        cout<<"\n student not found"<<endl;
+    }
 }
-cout << " printing completed!";
- f. close();
-}
-
-void SearchStudent ()
+void deletestudent()
 {
- ifstream f("db.txt") ;
+    ifstream f("db.txt");
+    string line,rn;
+    string filedata;
+    cout<<"\n enter the roll to be deleted:";
+    cin>>rn;
 
-string line,rn;
-
-cout<< "Enter Student Roll No. To be search:";
-cin>>rn;
-bool found = false;
- while (getline (f,line))
- {
-    if(line.find(rn)!=string::npos)
-{
-	cout<<"student found"<<endl;
-	cout<<"student details!!!"<<endl;
-	cout<<line<<endl;
-	found=true;
-	break;
+    while(getline(f,line))
+    {
+        if(line.find(rn)==string::npos)
+        {
+            filedata += line;
+            filedata += "\n";
+        }
+    }
+    f.close();
+    ofstream f1("db.txt", ios::out);
+    f1<<filedata;
+    f1.close();
 }
-}
-f.close();
-if(!found)
-{
-	cout << " Student Doesn't Exist "<< endl;
-}
-
-}
-
-void deletestudent () 
-{ 	ifstream f("db.txt");
-	string line,rn;
-	cout << " Enter Student Roll no. To deleted";
-	cin>>rn;
-	
-
-    while(getline (f, line))
- { 
-	    if (line.find (rn) == string::npos) 
- { 
-		    fileData += line;
-                    fileData += " \n";
-}
-}
- f.close();
- ofstream f1("db.txt",ios::out);
- f1<<filedata;
- f1.close();
-}
-
 int main()
 {
-  ofstream f("db.txt", ios:: out); 
- f<< left << setw(20) <<" Roll No" <<setw(20)<<" NAME " << setw(20)<<"Division"<<setw(20)<<"Address"<<endl;
- f.close();
- int ip;
- while(ip!=-1)
-{ 
-	 cout << "\n Enter your choice \n. 1.Add student\n 2. Delete Student \n 3.Search Student\n4.printdata\n5"<<endl;
+    ofstream f("db.txt",ios::out);
+   f<<left<<setw(20)<<"ROLL NO"<<setw(20)<<"DIV"<<setw(20)<<"NAME"<<setw(20)<<"ADDRESS"<<endl;
+   f.close();
+   int ip;
+   while(ip!=-1)
+   {
+       cout<<"\n enter your choice \n 1.add student\n 2.delete student\n 3.search student\n 4.print data\n.5.Exit";
+       cin>>ip;
+       switch(ip)
+       {
+       case 1:
+        addstudent();
+        break;
+       case 2:
+        deletestudent();
+        break;
+       case 3:
+        searchstudent();
+        break;
+       case 4:
+        printstudent();
+        break;
+       case 5:
+        return 0;
+        break;
+        default:
+        cout<<"\n please ReEnter your choice";
+        break;
+       }
 
-cin>> ip;
 
-switch (ip)
- { 
-	case 1:
- 	addStudent();
-	break; 
+   }
 
-        case 2:
-	deletestudent();
-	break; 
-
-        case 3:
- 	SearchStudent ();
- 	break; 
-
-        case 4:
-	printStudent();
-	break; 
-		
-        case 5:
-	return 0;
-	break;
-default:
-cout<< "please ReEnter your choice"<<endl;
-break;
-}
-}
 }
